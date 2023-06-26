fb90b34e4e5ccfdf5806e03073f9c1537900bf55
jireland607 <jireland@outlook.com>
2023-06-26 16:43:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+
+const app = express();
+app.use(bodyParser.text());
+
+app.put('/api/convert-to-pdf', (req, res) => {
+  const html = req.body;
+  
+  // Convert HTML to PDF
+  pdf.create(html).toBuffer((err, buffer) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error converting HTML to PDF');
+    } else {
+      // Convert PDF buffer to base64
+      const base64 = buffer.toString('base64');
+      res.send(base64);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
